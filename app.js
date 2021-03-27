edccb6c569ac6af2731a0f2d36e71ebaec65f700
meg768 <meg768@users.noreply.github.com>
2021-03-27 17:26:03 +0100

    -

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ class App {
 		yargs.option('host',     {describe:'Specifies MQTT host', default:process.env.MQTT_HOST});
 		yargs.option('password', {describe:'Password for MQTT broker', default:process.env.MQTT_PASSWORD});
 		yargs.option('username', {describe:'User name for MQTT broker', default:process.env.MQTT_USERNAME});
-		yargs.option('port',     {describe:'Port for MQTT', default:process.env.MQTT_PORT});
-		yargs.option('topic',    {describe:'MQTT root topic', default:process.env.MQTT_TOPIC});
-		yargs.option('debug',    {describe:'Debug mode', type:'boolean', default:process.env.DEBUG === '1'});
+		yargs.option('port',     {describe:'Port for MQTT', type:'number', default:parseInt(process.env.MQTT_PORT) || 1883});
+		yargs.option('interval', {describe:'Poll interval in minutes', type:'number', default:parseInt(process.env.INTERVAL) || 15});
+		yargs.option('topic',    {describe:'MQTT root topic', default:process.env.MQTT_TOPIC || 'RSS'});
+		yargs.option('debug',    {describe:'Debug mode', type:'boolean', default:parseInt(process.env.DEBUG) || false});
 
 		yargs.help();
 		yargs.wrap(null);
@@ -35,7 +36,6 @@ class App {
 		this.debug   = this.argv.debug ? this.log : () => {};
 		this.parser  = new Parser();
 		this.entries = {};
-		this.cache   = {};
 		this.timer   = new Timer();
 
 	}
@@ -69,91 +69,33 @@ class App {
 		let content = lastItem.contentSnippet;
 		let date = lastItem.isoDate;
 
-		let feed = {title:title, content:content, link:link, date:date};
-
-		this.debug(`Fetched ${url} - ${JSON.stringify(feed.title)}`);
-		return feed;
+		return {title:title, content:content, link:link, date:date};
     }
 
 
 	async fetch() {
-/*
-		for (const [url, name] of Object.entries(this.entries)) {
-			this.cache[url] = await this.fetchURL(url);
+
+		this.debug(`Fetching RSS feeds...`);
+
+		for (const [name, entry] of Object.entries(this.entries)) {
+			let feed = await this.fetchURL(entry.url);
+
+			if (entry.feed == undefined || JSON.stringify(entry.feed) != JSON.stringify(feed)) {
+
+				this.debug(`Feed ${name} changed.`);
+
+				Object.keys(feed).forEach((key) => {
+					this.publish(`${this.argv.topic}/${name}/${key}`, feed[key]);
+				});
+
+				entry.feed = feed;
+			}
 
 		}
-		return;
-*/
-/*
-		let promises = [];
 
-		Object.keys(this.entries).forEach(async (name) => {
-			let entry = this.entries[name];
-			promises.push(this.fetchURL(entry.url));
-		});
-
-		await Promise.all(promises);
-		return;
-*/
-		return new Promise((resolve, reject) => {
-			let promise = Promise.resolve();
-
-			Object.keys(this.entries).forEach((name) => {
-				let entry = this.entries[name];
-	
-				promise = promise.then(() => {
-					return this.fetchURL(entry.url);
-				})
-				.then((feed) => {
-					this.cache[entry.url] = feed;
-				});
-			});
-	
-			promise.then(() => {
-				resolve();
-			})
-	
-		});
-
-		
-		Object.keys(this.entries).forEach(async (name) => {
-			let entry = this.entries[name];
-			this.cache[entry.url] = await this.fetchURL(entry.url);
-		});
 	}
 
 
-	async update() {
-		this.debug(`Updating...`);
-
-		Object.keys(this.entries).forEach((name) => {
-			let entry = this.entries[name];
-			let cache = this.cache[entry.url];
-
-			if (cache != undefined) {
-				if (entry.cache == undefined || JSON.stringify(entry.cache) != JSON.stringify(cache)) {
-
-					this.debug(`Feed ${entry.url} changed.`);
-					this.debug(JSON.stringify(entry.url));
-					this.debug(JSON.stringify(cache.url));
-
-					Object.keys(cache).forEach((key) => {
-						this.publish(`${this.argv.topic}/${name}/${key}`, cache[key]);
-					});
-
-					entry.cache = cache;
-				}
-				else {
-					this.debug(`No change for url ${entry.url}.`);
-				}
-			}
-			else {
-				this.debug(`No cache for RSS feed ${entry.url}...`);
-			}
-		});		
-
-		  		
-	}
 
 	publish(topic, value) {
 		value = JSON.stringify(value);
@@ -163,10 +105,8 @@ class App {
 
 
 	async loop() {
-		this.log(`Updating RSS feeds...`);
 		await this.fetch();
-		await this.update();
-		setTimeout(this.loop.bind(this), 1000 * 60 * 15);
+		setTimeout(this.loop.bind(this), 1000 * 60 * this.argv.interval);
 	}
 
 	async run() {
@@ -193,11 +133,10 @@ class App {
 						try {
 							let config = JSON.parse(message);
 							this.log(`Added RSS feed ${args.name}:${JSON.stringify(config)}...`);
-							this.entries[args.name] = {url:config.url, name:args.name};
+							this.entries[args.name] = {url:config.url};
 
 							this.timer.setTimer(2000, async () => {
 								await this.fetch();
-								await this.update();
 							});
 						}
 						catch(error) {
