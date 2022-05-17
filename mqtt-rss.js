b0004932c047604877e06c5d220f1eac30e6278c
meg768 <meg768@users.noreply.github.com>
2022-05-17 17:47:05 +0200

    -

--- mqtt-rss.js
+++ mqtt-rss.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var MQTT   = require('mqtt-ex');
+var MQTT   = require('mqtt');
 var Parser = require('rss-parser');
 var Events = require('events');
 var Timer  = require('yow/timer');
@@ -14,17 +14,11 @@ class App {
 	constructor() {
 		var yargs = require('yargs');
 
-
 		yargs.usage('Usage: $0 [options]')
 
 		yargs.option('help',     {alias:'h', describe:'Displays this information'});
-		yargs.option('host',     {describe:'Specifies MQTT host', default:process.env.MQTT_HOST});
-		yargs.option('password', {describe:'Password for MQTT broker', default:process.env.MQTT_PASSWORD});
-		yargs.option('username', {describe:'User name for MQTT broker', default:process.env.MQTT_USERNAME});
-		yargs.option('port',     {describe:'Port for MQTT', type:'number', default:parseInt(process.env.MQTT_PORT) || 1883});
-		yargs.option('interval', {describe:'Poll interval in minutes', type:'number', default:parseInt(process.env.INTERVAL) || 15});
-		yargs.option('topic',    {describe:'MQTT root topic', default:process.env.MQTT_TOPIC || 'RSS'});
-		yargs.option('debug',    {describe:'Debug mode', type:'boolean', default:parseInt(process.env.DEBUG) || false});
+		yargs.option('config',   {describe:'Specifies JSON config file', default:'.config'});
+		yargs.option('debug',    {describe:'Debug mode', default:true});
 
 		yargs.help();
 		yargs.wrap(null);
@@ -33,17 +27,11 @@ class App {
 			return true;
 		});
 
-		this.config  = require('yow/config')('.config');
 		this.argv    = yargs.argv;
+		this.config  = require('yow/config')(this.argv.config);
 		this.log     = console.log;
 		this.debug   = this.argv.debug ? this.log : () => {};
-		this.parser  = new Parser();
-		this.feeds   = this.config.feeds;
 		this.cache   = {};
-		this.timer   = new Timer();
-
-
-	
 	}
 
 
@@ -51,7 +39,8 @@ class App {
 
 		this.debug(`Fetching ${url}...`);
 
-		let result = await this.parser.parseURL(url);
+		let parser = new Parser();
+		let result = await parser.parseURL(url);
 
 		result.items.sort((A, B) => {
 			let timeA = new Date(A.isoDate);
@@ -139,43 +128,8 @@ class App {
 			this.mqtt = MQTT.connect(this.config.host, {username:this.config.username, password:this.config.password, port:this.config.port});
 					
 			this.mqtt.on('connect', () => {
-				this.log(`Connected to host ${this.config.host}:${this.config.port}.`);
-
-				
+				this.debug(`Connected to host ${this.config.host}:${this.config.port}.`);
 			});
-
-			/*
-			this.mqtt.subscribe(`${this.argv.topic}/#`);
-
-			this.mqtt.on(`${this.argv.topic}/:name`, (topic, message, args) => {
-				try {
-					if (message == '') {
-						this.log(`Removed topic ${topic}...`);
-						delete this.entries[args.name];
-					}
-					else {
-						try {
-							let config = JSON.parse(message);
-							this.log(`Added RSS feed ${args.name}:${JSON.stringify(config)}...`);
-							this.entries[args.name] = {url:config.url};
-
-							this.timer.setTimer(2000, async () => {
-								await this.fetch();
-							});
-						}
-						catch(error) {
-							throw new Error(`Invalid configuration "${message}".`);
-						}
-					}
-
-	
-				}
-				catch(error) {
-					this.log(error);
-				}
-
-			});
-			*/
 
 			this.loop();
 
